d3fcbc71f3da508eb5220079d26c8118e87faf4f
dvlnaruto06 <koshtiharshil006@gmail.com>
2025-05-11 22:50:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', "25059287"))
-API_HASH = environ.get('API_HASH', "5e7701953107a273724b07f2beaf8f17")
+API_ID = int(environ.get('API_ID', ""))
+API_HASH = environ.get('API_HASH', "")
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 TIMEZONE = environ.get("TIMEZONE", "Asia/Kolkata")
 
